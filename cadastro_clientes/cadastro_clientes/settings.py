20620100478174cd989d41bc2b30480141e04418
Thomas Lopes <117936430+thomas-lp@users.noreply.github.com>
2024-08-28 16:56:48 -0300

    Update settings.py CONEXÃO BANCO RDS

--- cadastro_clientes/cadastro_clientes/settings.py
+++ cadastro_clientes/cadastro_clientes/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cadastro_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cadastro-clientes-db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'cadastro-clientes-db.c3am8gos2rlp.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
