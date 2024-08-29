27a6d21944e7bf1dd65f6d794f2be19bf99f77e0
Thomas Lopes <117936430+thomas-lp@users.noreply.github.com>
2024-08-29 14:41:32 -0300

    Update urls.py REDIRECIONAR /clientes/

--- cadastro_clientes/cadastro_clientes/urls.py
+++ cadastro_clientes/cadastro_clientes/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # Import necessário para o redirecionamento
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
+    path('', lambda request: redirect('/clientes/', permanent=True)),  # Redireciona a URL raiz para /clientes/
 ]
